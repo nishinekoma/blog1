10a957f23b73d4d2606a0938467c6529045f1356
24nekoma <nishi.kazu2488@gmail.com>
2025-11-12 20:12:24 +0900

    クロップ情報DB保存。Viewに表示可能。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,10 +25,7 @@ class SessionsController < ApplicationController
     redirect_to login_path, allow_other_host: true
   end
 
-  # #private
-  #   def user_params
-  #     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  #   end
+
 # --- singup function --- #
   def signup #View signup.html.erb
     #Userのモデルオブジェクトを生成
@@ -78,10 +75,14 @@ class SessionsController < ApplicationController
       @user = current_user
     end
 
-    private
-      def user_params
-        p "user_params is    \n", params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :image_icon)
-      end
-
-end
+  private
+    def user_params
+      p "user_params is \n", params
+      # ここにクロップ情報用の4つのパラメータを追加します
+      params.require(:user).permit(
+        :name, :email, :password, :password_confirmation, :image_icon,
+        #CropperJSから送られるhidden fieldのキーを追加
+        :image_x, :image_y, :image_w, :image_h
+      )
+    end
+  end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,10 +5,11 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-# Bootstrapをピン留め（HTMLのCDNと併用されることで安定動作）
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
-
 # 安定動作が確認された jspm.io の Cropper.js パス
 pin "cropperjs", to: "https://ga.jspm.io/npm:cropperjs@1.6.2/dist/cropper.esm.js"
+
+
+
+# Bootstrapをピン留め（HTMLのCDNと併用されることで安定動作）
+# pin "bootstrap", to: "bootstrap.min.js", preload: true
+# pin "@popperjs/core", to: "popper.js", preload: true
