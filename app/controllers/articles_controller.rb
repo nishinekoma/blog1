b8497d8143db332b9e6d0476c3a35f1dbe035857
nishineko <nishi.kazu2488@gmail.com>
2025-12-17 11:55:36 +0900

    from desktop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,40 +5,79 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new #新規記事を作成している　post
+    # 新規記事を作成している　post
+    @article = ArticleForm.new(user_id: session[:user_id])
   end
 
   def create
-    p "session[:user_id]    :",session[:user_id]
-    #現在ログインしているuser_idを格納
-    @user_id = User.find(session[:user_id])
-    p "create @article user.find(params[:user_id]) :",@user_id
-    #そのユーザに付属する記事を作成
-    #@article = @user_id.Article.new(article_params)
-    @article = @user_id.articles.new(article_params)
-    p "@article.Article.new(article_params) :",@article
-    if @article.save
-      redirect_to @article
+    #現在ログインしているuser_idを格納????
+    form_params_with_user = article_form_params.merge(user_id: session[:user_id])
+    # 送信されたフォームデータを基にArticleFormオブジェクトを初期化
+    @article_form = ArticleForm.new(form_params_with_user)
+    p "create @article_form :",@article_form
+
+    #ArticleFormのsaveメソッドを呼び出し、ArticleとContentBlockの保存処理を実行
+    if @article_form.save
+      # 保存成功時、作成された記事のIDを取得してリダイレクト
+      # フォームオブジェクトが保存した記事のIDを知る必要がある（例：ArticleFormにarticleを格納する）
+      # ここでは ArticleForm#save が成功したら、そのインスタンスに保存された記事情報（IDなど）があることを前提とする
+      redirect_to @article_form.article
     else
-      p "article failed",@article.errors.full_messages
-
-      render :new, status: :unprocessable_entity #エラー時のメッセージ　clientが送信したリクエスト処理できないトキイに使用されえる。
-
+      p "article form failed", @article_form.errors.full_messages
+      # エラー時は :new テンプレートを再描画
+      render :new, status: :unprocessable_entity
     end
+
+
+    # p "session[:user_id]    :",session[:user_id]
+    # #現在ログインしているuser_idを格納
+    # @user_id = User.find(session[:user_id])
+    # p "create @article user.find(params[:user_id]) :",@user_id
+    # #そのユーザに付属する記事を作成
+    # #@article = @user_id.Article.new(article_params)
+    # @article = @user_id.articles.new(article_params)
+    # p "@article.Article.new(article_params) :",@article
+    # if @article.save
+    #   redirect_to @article
+    # else
+    #   p "article failed",@article.errors.full_messages
+
+    #   render :new, status: :unprocessable_entity #エラー時のメッセージ　clientが送信したリクエスト処理できないトキイに使用されえる。
+    # end
+
   end
 
-  def edit #DBから記事を取得 @articleに保存
-    @article = Article.find(params[:id]) #既存のユーザを参照している　patch
+  # 記事編集
+  def edit
+    # 既存の記事IDを基にArticleFormオブジェクトを初期化
+    # ArticleFormのinitialize内で、既存の記事とContentBlockを取得する
+    @article = ArticleForm.new(id: params[:id]) #既存のユーザを参照している　patch
   end
 
+  # 記事更新
   def update
-    @article = Article.find(params[:id])#DBから再取得　既存のユーザを参照している　patch
-    p "#{params[:id]}      #{article_params}"
-    if @article.update(article_params)
-      redirect_to @article
+    # 既存の記事IDと送信されたパラメータを組み合わせてフォームオブジェクトを初期化
+    form_params_with_id = article_form_params.merge(user_id: session[:user_id])
+
+    @article_form = ArticleForm.new(form_params_with_id)
+    p "update @article_form:", @article_form
+
+    #フォームオブジェクトのsaveメソッドを呼び出し
+    if @article_form.save
+      # 保存成功後、更新された記事ページへリダイレクト
+      # ArticleFormに保存した記事のIDがあることを前提とする
+      redirect_to @article_form.article
     else
+      # エラー時は :edit テンプレートを再描画
       render :edit, status: :unprocessable_entity
     end
+    # @article = Article.find(params[:id])#DBから再取得　既存のユーザを参照している　patch
+    # p "#{params[:id]}      #{article_params}"
+    # if @article.update(article_params)
+    #   redirect_to @article
+    # else
+    #   render :edit, status: :unprocessable_entity
+    # end
   end
 
   def destroy
@@ -49,8 +88,14 @@ class ArticlesController < ApplicationController
   end
   
   private #ArticlesControllerのみで使用可能にし、外部からのアクセスをなくす。
-    def article_params
-      params.require(:article).permit(:title, :body, :status)
+    def article_form_params
+      params.require(:article).permit(
+        :id,
+        :title,
+        :summary,
+        # ContentBlockのネストされた属性を受け取る
+        content_blocks_attributes: [:id, :block_type, :content, :image_url, :position, :_destroy]
+      )
     end
 end
 
--- app/forms/article_form.rb
+++ app/forms/article_form.rb
@@ -23,31 +23,95 @@ class ArticleForm
 
   #初期化メソッド　引数なしで渡される場合もあるのでデフォルト値を設定
   def initialize(attributes = {})
-    #Attributeが呼び出される。自動初期化ロジックが上書きされるのを防ぐためsuperで元のロジックを呼び出す
-    #自動初期化ロジックは、attributesハッシュのキーと一致する属性(attribute)を探し、それらに対応する値を設定する
+    # Attributeが呼び出される。自動初期化ロジックが上書きされるのを防ぐためsuperで元のロジックを呼び出す
+    # 自動初期化ロジックは、attributesハッシュのキーと一致する属性(attribute)を探し、それらに対応する値を設定する
     super(attributes)
     # フォームの編集時（idが存在する場合）は、既存の content_blocksを設置 
-    #db/schemaにArticleFormにはcontent_blocksテーブルは存在しないが、ArticleFormクラス内でcontent_blocks_attributesとして定義されている
-    #content_blocks_attributesはattr_accessorで定義されているため、ArticleFormのインスタンス変数として利用可能
-    #リクエストごとにcontrollerからユーザが現在いるフォームの複数のデータを受け取り、ArticleForm.newされることでpresent?を判定できる。
+    # db/schemaにArticleFormにはcontent_blocksテーブルは存在しないが、ArticleFormクラス内でcontent_blocks_attributesとして定義されている
+    # content_blocks_attributesはattr_accessorで定義されているため、ArticleFormのインスタンス変数として利用可能
+    # リクエストごとにcontrollerからユーザが現在いるフォームの複数のデータを受け取り、ArticleForm.newされることでpresent?を判定できる。
     if self.id.present?
-      #既存の記事取得、紐づいているContentBlockも取得
+      # 既存の記事取得、紐づいているContentBlockも取得
       article = Article.find(self.id)
-      #Article.idに紐づいている複数のContentBlockを取得し格納
+      # Article.idに紐づいている複数のContentBlockを取得し格納
       self.content_blocks_attributes = article.content_blocks.map do | block |
         # 既存のブロックをハッシュ形式でセット
         block.attributes.slice('id', 'block_type', 'content', 'image_url', 'position')
       end
     else
-      #新規作成時はからのブロックリストを初期化 ||= はself.content_blocks_attributesが未定義なら[]を代入する
+      # 新規作成時はからのブロックリストを初期化 ||= はself.content_blocks_attributesが未定義なら[]を代入する
       self.content_blocks_attributes ||= []
     end
   end
   
-  # 保存メソッド
+  # Article と ContentBlock の保存処理
   def save
-    #バリデーションチェック
+    # バリデーションチェック
     return false unless valid?
 
-    #
+    # トランザクション開始
+    Article.transactionon do
+
+      # Articleの保存/更新
+      # self.id が存在する場合（既存編集）は検索してインスタンスを取得し、
+      # 存在しない場合（新規作成）はIDを持たない新しいインスタンスを作成。
+      article = Article.find_or_initialize_by(id: self.id)
+      #attributesメソッドを使用してArticleの属性をフォームの値で更新(上書き)
+      article.attributes = {
+        title: self.title,
+        summary: self.summary,
+        user_id: self.user_id
+      }
+      article.save!
+
+      # ContentBlockの保存/更新/削除
+      save_content_blocks(article)
+
+      # 保存した記事インスタンスをフォームオブジェクト内に保持して返す
+      @article = article
+    end
+    #トランザクションが成功したらtrueを返す
+    true
+
+    # エラーハンドリング（必要に応じてエラーメッセージを ArticleForm に移す処理を実装）
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound => e
+      Rails.logger.error "Form saving failed: #{e.message}"
+      false
   end
+
+  # ContentBlockの保存/更新/削除処理
+  def save_content_blocks(article)
+    
+    # ContentBlockのattributesを処理
+    # positionはフォームから送信された順番に基づいて設定
+    self.content_blocks_attributes.each_with_index do | block_attr, index |
+      # 文字列とシンボルのどちらをキーに指定してもアクセスできるように変換　例：{ a: 1 }.with_indifferent_access["a"] # => 1　
+      block_attr = block_attr.with_indifferent_access
+
+      # ブロックが削除フラグ付きの場合かつidが存在する場合
+      if block_attr[:_destroy] == '1' && block_attr[:id].present?
+        # dbに保存されている既存のContentBlockを検索して削除
+        content_block = article.content_blocks.find(block_attr[:id])
+        content_block.destroy
+        # コンテンツは削除されたため、次のループへ
+        next
+      end
+
+      # ブロックの作成または更新
+      # positionはループのインデックスを使用して設定
+      block_attr [:position] = index
+
+      # 既存ブロックの更新
+      if block_attr[:id].present?
+        # 更新したいarticleに紐づいている全てのcontent_blockレコードをidで検索して取得。idで検索するためfindを使用
+        content_block = article.content_blocks.find(block_attr[:id])
+        #id, _destroyキーは不要なので除外して更新
+        content_block.update!(block_attr.except(:id, :_destroy)) 
+      else
+        # 新規ブロックの作成
+        article.content_blocks.create!(block_attr.except(:_destroy))
+      end
+
+    end #end content_blocks_attributes.each_with_index
+  end #end def save_content_blocks
+end #end class ArticleForm
